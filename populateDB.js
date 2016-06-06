9e28df82f8072aa428ac9032edecd08c3acc2094
Octavio Anastasio Menocal Cordero <Octavio Anastasio Menocal Cordero>
2016-06-05 22:47:52 -0600

    Changed database json file

--- populateDB.js
+++ populateDB.js
@@ -6,38 +6,52 @@ AWS.config.update({
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
-var index = 0;
 var carNumberId = 0;
+var numberOfItemToExport = process.argv[2];
 
-console.log("Importing items into DynamoDB.");
+if(numberOfItemToExport !== undefined) {
+  numberOfItemToExport = numberOfItemToExport * 500;
+} else {
+  numberOfItemToExport = 500;
+}
+
+var init = numberOfItemToExport - 500;
 
 var allItems = JSON.parse(fs.readFileSync('json_custom_export_final.json', 'utf8'));
-allItems.forEach(function(car) {
-    
-    car.Items.forEach(function(item) {
-        carNumberId ++;
 
-        var params = {
-            TableName: "Cars",
-            Item: {
-                "CarId": "" + carNumberId,
-                "Brand": car.Brand.toLowerCase(),
-                "Model": car.Model.toLowerCase(),
-                "Year": car.Year,
-                "Title": item.Titulo.toLowerCase(),
-                "Description": item.Description.toLowerCase()
-            }
-        };
+console.log("Importing 500 items out of " + allItems.length + " into DynamoDB. Extra info: init = " + init + " numberOfItemToExport = " + numberOfItemToExport);
 
-        docClient.put(params, function(err, data) {
-           index ++;
-           if (err) {
-               console.error("Unable to add item", car.Brand + "-" + car.Model + "-" + car.Year,
-                            ". Error JSON:", JSON.stringify(err, null, 2));
-           } else {
-               console.log("PutItem succeeded[" + index + "]:", car.Brand + "-" + car.Model + "-" + car.Year);
-           }
-        });
+if(numberOfItemToExport > allItems.length) {
+  numberOfItemToExport = allItems.length;
+}
+
+for (var i = init; i < numberOfItemToExport; i++) {
+  var car = allItems[i];
+
+  for (var j = 0; j < car.items.length; j++) {
+    var item = car.items[j];
+
+    var params = {
+        TableName: "Cars",
+        Item: {
+            "CarId": "" + i,
+            "Brand": car.marca.toLowerCase(),
+            "Model": car.modelo.toLowerCase(),
+            "Year": car.anio,
+            "Title": item.titulo.toLowerCase(),
+            "Description": item.descripcion.toLowerCase()
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add item", car.marca + "-" + car.modelo + "-" + car.anio,
+                        ". Error JSON:", JSON.stringify(err, null, 2));
+           return;
+       } else {
+           console.log("PutItem succeeded[" + i + "]:", car.marca + "-" + car.modelo + "-" + car.anio);
+       }
     });
-    
-});
+  }
+}
+
