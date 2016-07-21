3453c242400d186cbbbeefa792d5f7d084ed5f5d
omenocal <octavio_menocal92@hotmail.es>
2016-07-21 07:22:21 -0600

    Improved populate script

--- populateDB.js
+++ populateDB.js
@@ -31,36 +31,69 @@ if(numberOfItemToExport > allItems.length) {
   numberOfItemToExport = allItems.length;
 }
 
-for (var i = init; i < numberOfItemToExport; i++) {
-  var car = allItems[i];
+insertItems(init, numberOfItemToExport, allItems);
 
-  for (var j = 0; j < car.items.length; j++) {
-    var item = car.items[j];
+function insertItems(i, numberOfItemToExport, allItems) {
+    if(i >= numberOfItemToExport) {
+        return;
+    }
 
-    var params = {
+    var car = allItems[i];
+
+    var scanParams = {
         TableName: "Cars",
-        Item: {
-            "CarId": "" + i,
-            "Brand": car.marca.toLowerCase(),
-            "Model": car.modelo.toLowerCase(),
-            "Year": car.anio,
-            "Title": item.titulo.toLowerCase(),
-            "Description": item.descripcion.toLowerCase()
+        ProjectionExpression: "#brand, #model, #year",
+        FilterExpression: "#brand = :brand AND #model = :model AND #year = :year",
+        ExpressionAttributeNames: {
+            "#brand": "Brand",
+            "#model": "Model",
+            "#year": "Year"
+        },
+        ExpressionAttributeValues: {
+            ":brand": car.marca.toLowerCase(),
+            ":model": car.modelo.toLowerCase(),
+            ":year": "" + car.anio
         }
     };
 
-    docClient.put(params, function(err, data) {
-       if (err) {
-           console.error("Unable to add item", car.marca + "-" + car.modelo + "-" + car.anio,
-                        ". Error JSON:", JSON.stringify(err, null, 2));
-           return;
-       } else {
-           console.log("PutItem succeeded[" + i + "]:", car.marca + "-" + car.modelo + "-" + car.anio);
-       }
-    });
-  }
+    docClient.scan(scanParams, function (errorScan, dataScan) {
+        if (errorScan) {
+            console.error("Unable to scan item", car.marca + "-" + car.modelo + "-" + car.anio,
+                                    ". Error JSON:", JSON.stringify(errorScan, null, 2));
+            insertItems(i + 1, numberOfItemToExport, allItems);
+        } else {
+            console.log("JSON.stringify(dataScan) = " + JSON.stringify(dataScan));
 
-  fs.appendFile('LIST_OF_BRANDS', car.marca + "\n", function (err) {});
-  fs.appendFile('LIST_OF_MODELS', car.modelo + "\n", function (err) {});
+            if(dataScan && dataScan.Items.length > 0) {
+                console.log("Item already in DynamoDB[" + i + "]:", car.marca + "-" + car.modelo + "-" + car.anio);
+            } else {
+                var params = {
+                    TableName: "Cars",
+                    Item: {
+                        "CarId": "" + i,
+                        "Brand": car.marca.toLowerCase(),
+                        "Model": car.modelo.toLowerCase(),
+                        "Year": car.anio,
+                        "Battery": car.items
+                    }
+                };
+
+                docClient.put(params, function(err, data) {
+                   if (err) {
+                       console.error("Unable to add item", car.marca + "-" + car.modelo + "-" + car.anio,
+                                    ". Error JSON:", JSON.stringify(err, null, 2));
+                       return;
+                   } else {
+                       console.log("PutItem succeeded[" + i + "]:", car.marca + "-" + car.modelo + "-" + car.anio);
+                   }
+                });
+            }
+
+            insertItems(i + 1, numberOfItemToExport, allItems);
+        }
+    });
+
+    fs.appendFile('LIST_OF_BRANDS', car.marca + "\n", function (err) {});
+    fs.appendFile('LIST_OF_MODELS', car.modelo + "\n", function (err) {});
 }
 
